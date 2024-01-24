d7db058e88bfea612026dd45e3d994ab4a385fc9
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2024-01-24 04:56:29 -0800

    0x06-python-classes TASKS 1-3 and READme update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Define a Class Square """
+
+class Square:
+    """ Repesents a Square """
+
+    def __init__(self, size):
+        """ Initialize a New Square.
+
+        Args:
+            size (int): The Size of the New Square.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Define a Class Square """
+
+class Square:
+    """ Represents a Square """
+
+    def __init__(self, size=0):
+        """ Initalize a New Sqaure.
+
+        Args:
+            size (int): The size of the new Sqaure.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
